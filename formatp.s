e37bdf418c0c6e33dc0999e3c1d4003671525d1b
LeonidYutlin <leonidyutlin@phystech.edu>
2026-03-22 21:12:23 +0300

    %c and %%, self-formatted errors

--- formatp.s
+++ formatp.s
@@ -2,7 +2,7 @@ global formatp
 extern strlen
 extern clear_buffer
 
-BUF_SZ equ 16
+BUF_SZ equ 64
 
 section .bss
 formatp_buf: resb BUF_SZ
@@ -51,12 +51,21 @@ Num2Str:
   ret
 
 formatp:
-  .convert:
-    mov rax, rdi
-  	mov rdi, formatp_buf
-  	call Num2Str
+  pop r15 ; save return address since we will iterate through stack
+
+  push r9  ; push register args to stack so that all 
+  push r8  ; args are on stack in order
+  push rcx ; from top to bottom
+  push rdx ; as first to last
+  push rsi ; for reference, r9 is sixth arg, and 
+  push rdi ; rdi is the first arg (fmt str)
+
+  push rbp ; save rbp
+  mov rbp, rsp
+  mov rsi, rdi ; move fmt str to source index
+  call handle_fmt_str
  
-  .print:
+  .return:
     mov rdi, formatp_buf
     call strlen ; call to a libc function
 
@@ -65,23 +74,60 @@ formatp:
   	mov rsi, formatp_buf
   	mov rdi, 0x1
   	syscall
+    mov rdi, formatp_buf
+    call clear_buffer ; call to my own function in main.c
+
+    pop rbp
+
+    add rsp, 8 * 6 ; equivalent to pop rdi, rsi, ..., r9
+    push r15 ; push saved ret address onto the stack
+    ret
+
+handle_fmt_str:
+  mov r13, 2 ; how many 8 offsets are we into the stack
+  mov rdi, formatp_buf
+  .loop:
+    mov al, [rsi]
+    cmp al, '%'
+    jne .not_escape
+      inc rsi
+      mov al, [rsi]
+      inc rsi
+      cmp al, 'c'
+      jne .not_char
+      mov al, [rbp + r13 * 8]
+      inc r13
+      stosb
+      jmp .loop
+      .not_char:
+      cmp al, '%'
+      jne .error
+      mov al, '%'
+      stosb
+      jmp .loop
+    .not_escape:
+    cmp al, 0
+    je .return
+    movsb
+  jmp .loop
+  .return:
+    ret
+  .error:
+    push rax
 
     mov rdi, formatp_buf
-    mov rsi, rdx ; still using that strlen return as count
-                 ; since it is not changed by prev syscall
-    call clear_buffer ; call to my own function in main.c
-  ret
+    call clear_buffer
 
-formatp1:
-  .convert:
-    mov rax, rdi
-  	mov rdi, formatp_buf
-  	call Num2Str
- 
-  .print:
-  	mov rdx, rcx
-  	mov rax, 0x1
-  	mov rsi, formatp_buf
-  	mov rdi, 0x1
-  	syscall
-  ret
+    mov rsi, fmt_error_str
+    push rsi
+    push rbp ; save rbp
+    mov rbp, rsp
+    call handle_fmt_str
+    pop rbp
+    add rsp, 8 * 2
+    ret
+
+section .data
+
+fmt_error_str: db 0x0A, "[ERROR]: Unrecognized escape sequence: '%%%c'", 0x0A
+fmt_error_str_len equ $ - fmt_error_str
--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-extern void formatp(long a);
+extern void formatp(const char* fmt, ...);
 
 //Example of a function that will be called 
 //from assembly to clear the buffer
-void clear_buffer(void* buf, size_t count) {
-  memset(buf, 0, count);
+void clear_buffer(char* buf) {
+  if (!buf) return;
+  while (*buf)
+    *buf++ = '\0';
 }
 
 int main(void) {
@@ -17,10 +19,8 @@ int main(void) {
   //        'A', 
   //        "Wello Horld!", 
   //        123);
-  formatp(8);
-  formatp(234);
-  formatp(4444444444444444444); 
-  //BUG: you can overflow the buffer with big numbers
-  formatp(11);
+  formatp("Hi %% I %c m %% atring\n", 'W');
+  formatp("Qwerty %% %c %c %c", 'A', 'p', 'D');
+  formatp("Hello blabla %% %A", 'A', 'p', 'D');
   return 0;
 }
